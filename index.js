dc1daf999bd0b17b6dbf00fca03b73d8c1992e60
Jason Liew <jason.97.liew@gmail.com>
2020-10-30 14:44:15 +1100

    Error handling

--- index.js
+++ index.js
@@ -20,35 +20,20 @@ const unknownEndpoint = (req, res, next) => {
 	res.status(404).json({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (err, req, res, next) => {
+	console.error(err.message)
+
+	if (err.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(err)
+}
+
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('build'))
-
-/**
- * Data
- */
-
-let notes = [
-	{
-		id: 1,
-		content: "HTML is easy",
-		date: "2019-05-30T17:30:31.098Z",
-		important: true
-	},
-	{
-		id: 2,
-		content: "Browser can execute only Javascript",
-		date: "2019-05-30T18:39:34.091Z",
-		important: false
-	},
-	{
-		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		date: "2019-05-30T19:20:14.298Z",
-		important: true
-	}
-]
 
 /**
  * Routes
@@ -62,15 +47,24 @@ app.get('/api/notes', (req, res) => {
 	Note.find({}).then(notes => res.json(notes))
 })
 
-app.get('/api/notes/:id', (req, res) => {
-	Note.findById(req.params.id).then(note => res.json(note))
+app.get('/api/notes/:id', (req, res, next) => {
+	Note.findById(req.params.id)
+		.then(note => {
+			if (note) {
+				res.json(note)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(err => next(err))
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-	const id = Number(req.params.id)
-	notes = notes.filter(note => note.id !== id)
-
-	res.status(204).end()
+	Note.findByIdAndDelete(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(err => next(err))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -91,7 +85,26 @@ app.post('/api/notes', (req, res) => {
 	note.save().then(savedNote => res.json(savedNote))
 })
 
+app.put('/api/notes/:id', (req, res, next) => {
+	const body = req.body
+
+	const note = {
+		content: body.content,
+		important: body.important
+	}
+
+	Note.findByIdAndUpdate(req.params.id, note, { new: true })
+		.then(updatedNote => {
+			res.json(updatedNote)
+		})
+		.catch(err => next(err))
+})
+
+// Handle requests with unknown endpoint
 app.use(unknownEndpoint)
+
+// Handle requests which result in errors
+app.use(errorHandler)
 
 /**
  * Bind to port and listen for requests
