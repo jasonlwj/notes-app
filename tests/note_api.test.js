cf2dd9e0ab698c6ce1c13a56a63ca1d3b4d5a8f0
Jason Liew <jason.97.liew@gmail.com>
2020-11-10 15:13:14 +1100

    Install supertest

---
+++ tests/note_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+	await api
+		.get('/api/notes')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+	const res = await api.get('/api/notes')
+	expect(res.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+	const res = await api.get('/api/notes')
+	expect(res.body[0].content).toBe('HTML is easy')
+})
+
+afterAll(() => {
+	mongoose.connection.close()
+})
