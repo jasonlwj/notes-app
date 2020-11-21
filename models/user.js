155ea364688b0623fd0964e022b8439c2018e49d
Jason Liew <jason.97.liew@gmail.com>
2020-11-22 10:28:48 +1100

    Add tests for user creation

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-	username: String,
+	username: {
+		type: String,
+		unique: true
+	},
 	name: String,
 	passwordHash: String,
 	notes: [
@@ -20,5 +24,7 @@ userSchema.set('toJSON', {
 		delete returnedObject.passwordHash
 	}
 })
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', userSchema)
