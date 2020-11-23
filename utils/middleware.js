f95fc7803a85b20eb9b0ee2dcbdc69a05230d63a
Jason Liew <jason.97.liew@gmail.com>
2020-11-23 20:40:47 +1100

    Update error handler to address invalid token

--- utils/middleware.js
+++ utils/middleware.js
@@ -16,9 +16,17 @@ const errorHandler = (err, req, res, next) => {
 	logger.error(err.message)
 
 	if (err.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' })
+		return res
+			.status(400)
+			.send({ error: 'malformatted id' })
 	} else if (err.name === 'ValidationError') {
-		return res.status(400).send({ error: err.message })
+		return res
+			.status(400)
+			.send({ error: err.message })
+	} else if (err.name === 'JsonWebTokenError') {
+		return res
+			.status(401)
+			.json({ error: 'invalid token' })
 	}
 
 	next(err)
